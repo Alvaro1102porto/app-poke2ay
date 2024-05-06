64d21eda1fe8cc560697f31afa32325f85006b5d
Alvaro1102porto <alvaroporto.1102@gmail.com>
2024-05-06 08:28:10 -0300

    pokemon listagem

---
+++ src/listagem/lista_pokemon.jsx
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from "react";
+
+const Listagem = () => {
+    <div>
+        <ul>
+            <li>Pokemòn 1</li>
+            <li>Pokemòn 2</li>
+            <li>Pokemòn 3</li>
+            <li>Pokemòn 4</li>
+            <li>Pokemòn 5</li>
+        </ul>
+    </div>
+}
+
+export default Listagem
